907d9548fb6f7b27aaf0635f20878447dd99908a
taghreedaa <taghreedMKamel@gmail.com>
2018-03-28 20:54:33 +0200

    Users and drivers migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,31 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+
             $table->string('password');
+            
+            $table->string('profile_picture')->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('family_name')->nullable();
+
+            $table->string('avatar')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->integer('gender')->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('bio')->nullable();
+
+            $table->integer('country_id')->unsigned();
+            $table->string('phone', 25);
+            $table->boolean('email_verified')->default(0);
+            $table->string('email_verify_code')->nullable();
+            $table->string('email')->unique();
+
+            $table->string('google_id')->unique()->nullable();
+            $table->string('facebook_id')->unique()->nullable();
+            $table->timestamp('last_active_at')->default(\Carbon\Carbon::now());
+
+
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2018_03_28_183017_create_drivers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+
+            $table->string('car_number');
+            $table->string('car_color');
+            $table->string('car_model');
+            $table->year('manufacturing_year');
+
+            $table->string('licence_number');
+            $table->date('licence_expire_date');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
